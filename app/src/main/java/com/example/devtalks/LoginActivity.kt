a7ed78152added0bb8856e1f4e6c0ea1a83760f2
Prateek Tiwari <prateektiwari5399@gmail.com>
2019-03-30 03:04:52 +0530

    Connected to firebase and Rounded the button

--- app/src/main/java/com/example/devtalks/LoginActivity.kt
+++ app/src/main/java/com/example/devtalks/LoginActivity.kt
@@ -3,19 +3,32 @@ package com.example.devtalks
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity:AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-     setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login)
+     login_button_login.setOnClickListener {
+         val email=email_edittext_login.text.toString()
+         val password=password_edittext_login.text.toString()
+         Log.d("Login","Login using email:$email , password $password")
+         FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+             .addOnCompleteListener {
+                 if (!it.isSuccessful)return@addOnCompleteListener
+                 Log.d("Login","Loged in using Email and password with uid ${it.result?.user?.uid}")
+             }
+             .addOnFailureListener {
+                 Log.d("Login","Failure with uid: ${it.message}")
+             }
+     }
 
-        already_have_account_textView.setOnClickListener {
-//            val i=Intent(this,MainActivity::class.java)
-            val email=email_edittext_login.text.toString()
-            val password=password_edittext_login.text.toString()
+//    Takes back to the initial screen when clicked on "Back to register text"
+        back_to_register_textview.setOnClickListener {
             finish()
         }
-    }
+        }
 }
--- app/src/main/java/com/example/devtalks/MainActivity.kt
+++ app/src/main/java/com/example/devtalks/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.devtalks
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +13,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val email=email_edittext_register.text.toString()
-        val password=password_edittext_register.text.toString()
-        register_button_register.setOnClickListener {  }
-        already_have_account_textView.setOnClickListener {
-            val intent=Intent(this,LoginActivity::class.java)
-            startActivity(intent)
+        register_button_register.setOnClickListener {
+            performRegister()
         }
+        // To open next activity ie login screen if user already has an account
+    already_have_account_textView.setOnClickListener{
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Enter your credentials to continue", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity", "Email is $email")
+        Log.d("MainActivity", "Password is :$password")
+//    Firebase authentication to create user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                Log.d("Main", "Sucessfully created user using uid: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Failiure ${it.message}")
+                Toast.makeText(this,"Failure!! ${it.message}",Toast.LENGTH_SHORT).show()
+            }
+    }
+}
+
